7946c35aa5c646918b73a5273001691e8b4f990a
Dave Nicponski <dave.nicponski@gmail.com>
2021-03-03 00:15:36 -0500

    Fix typo in CanBind.scala

--- core/shared/src/main/scala/scalapb/zio_grpc/CanBind.scala
+++ core/shared/src/main/scala/scalapb/zio_grpc/CanBind.scala
@@ -4,7 +4,7 @@ import zio.Has
 
 // Represents evidence that a service with Context C can be bound (that is, we can
 // generate a ServerServiceDefinition for it). To be able to bind a service with Context C
-// we need to be able to convert a RequsetContext to C.
+// we need to be able to convert a RequestContext to C.
 trait CanBind[C] {
   def bind(in: Has[RequestContext]): C
 }
